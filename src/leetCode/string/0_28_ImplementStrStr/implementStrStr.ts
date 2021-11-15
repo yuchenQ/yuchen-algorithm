81372236bfa97e001a9c8790ca25c5e541f50fd6
yuchenQ <qiaoycmelbourne@gmail.com>
2021-11-15 16:54:10 +1100

    0 28 implement strstr

---
+++ src/leetCode/string/0_28_ImplementStrStr/implementStrStr.ts
@@ -0,0 +1,42 @@
+// https://leetcode-cn.com/problems/implement-strstr/
+function strStr(haystack: string, needle: string): number {
+  if (needle === "") {
+    return 0;
+  }
+
+  if (haystack === "" || haystack.length < needle.length) {
+    return -1;
+  }
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (
+      haystack.charAt(i) === needle.charAt(0) &&
+      haystack.substring(i, i + needle.length) === needle
+    ) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+
+function strStr1(haystack: string, needle: string): number {
+  return haystack.indexOf(needle);
+};
+
+function strStr2P(haystack: string, needle: string): number {
+  if (needle == "") return 0;
+
+  let i = 0, j = 0;
+
+  while (i < haystack.length) {
+    if (haystack[i] === needle[j]) {
+      if (j === needle.length - 1) return i - j;
+      i += 1, j += 1;
+    } else {
+      i = i - j + 1, j = 0;
+    }
+  }
+
+  return -1;
+}
