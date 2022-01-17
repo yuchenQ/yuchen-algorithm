fab6146dcca6010c8e33e0872bf50014601095d2
yuchenQ <qiaoycmelbourne@gmail.com>
2022-01-17 19:40:04 +1100

    mock api call and process nested obj

---
+++ src/others/fetchAndMockRes/processData.js
@@ -0,0 +1,115 @@
+const dataset = [
+  { shapeId: "basic-shape", color: { r: 1, g: 1, b: 1 }, children: [] },
+  {
+    shapeId: "duck",
+    color: { r: 1, g: 1, b: 1 },
+    children: [
+      {
+        shapeId: "duck-bill",
+        color: { r: 1, g: 1, b: 1 },
+        children: [
+          {
+            shapeId: "duck-bill-1",
+            color: { r: 1, g: 1, b: 1 },
+            children: [],
+          },
+          {
+            shapeId: "duck-bill-2",
+            color: { r: 1, g: 1, b: 1 },
+            children: [],
+          },
+        ],
+      },
+      { shapeId: "duck-body", color: { r: 1, g: 1, b: 1 }, children: [] },
+      { shapeId: "duck-legs", color: { r: 1, g: 1, b: 1 }, children: [] },
+    ],
+  },
+];
+
+const mockFetch = () => Promise.resolve(dataset);
+
+const calcAverageByRecursion = (dataset) => {
+  const result = {
+    r: 0,
+    g: 0,
+    b: 0,
+    count: 0,
+  };
+
+  const recursion = (shape) => {
+    const { color, children } = shape;
+
+    result.r += color.r;
+    result.g += color.g;
+    result.b += color.b;
+    result.count += 1;
+
+    if (children.length !== 0) {
+      for (const child of children) {
+        recursion(child);
+      }
+    }
+  };
+
+  for (const shape of dataset) {
+    recursion(shape);
+  }
+
+  const avg = {
+    r: Math.round(result.r / result.count),
+    g: Math.round(result.g / result.count),
+    b: Math.round(result.b / result.count),
+  };
+
+  return {
+    result,
+    avg,
+  };
+};
+
+const calcAverage = (dataset) => {
+  const result = {
+    r: 0,
+    g: 0,
+    b: 0,
+    count: 0,
+  };
+
+  const queue = [...dataset];
+
+  while(queue.length) {
+    const shape = queue.shift();
+
+    const { color, children } = shape;
+
+    result.r += color.r;
+    result.g += color.g;
+    result.b += color.b;
+    result.count += 1;
+
+    if (children.length) {
+      queue.push(...children);
+    }
+  }
+
+  const avg = {
+    r: Math.round(result.r / result.count),
+    g: Math.round(result.g / result.count),
+    b: Math.round(result.b / result.count),
+  };
+
+  return {
+    result,
+    avg,
+  };
+}
+
+console.log(calcAverageByRecursion(dataset));
+console.log(calcAverage(dataset));
+
+mockFetch().then((dataset) => {
+  console.log(calcAverageByRecursion(dataset));
+});
+mockFetch().then((dataset) => {
+  console.log(calcAverage(dataset));
+});
