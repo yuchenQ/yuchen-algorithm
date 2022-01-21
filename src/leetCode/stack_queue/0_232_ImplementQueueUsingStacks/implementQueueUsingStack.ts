3afbafb460bd3d631fc21e4aec0977d4e5bc3f08
yuchenQ <qiaoycmelbourne@gmail.com>
2022-01-21 11:57:48 +1100

    0 232 implemet queue using stack

---
+++ src/leetCode/stack_queue/0_232_ImplementQueueUsingStacks/implementQueueUsingStack.ts
@@ -0,0 +1,31 @@
+class MyQueue {
+  private body: number[]
+    constructor() {
+      this.body = [];
+    }
+
+    push(x: number): void {
+      this.body.push(x);
+    }
+
+    pop(): number {
+      return this.body.shift();
+    }
+
+    peek(): number {
+      return this.body[0];
+    }
+
+    empty(): boolean {
+      return this.body.length ? false : true;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
